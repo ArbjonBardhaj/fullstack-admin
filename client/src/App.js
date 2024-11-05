e503bdf3938fe9864d38530195e55c21205ed315
ArbionBardhaj <bardhajarbion@gmail.com>
2024-11-05 17:00:00 +0100

    dockerized-version

--- client/src/App.js
+++ client/src/App.js
@@ -22,7 +22,7 @@ function App() {
   const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
   return (
     <div className="app">
-      <BrowserRouter>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
         <ThemeProvider theme={theme}>
           <CssBaseline />
           <Routes>
